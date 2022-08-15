eabbf272be01fa82269682b596bfc9c359b67604
Antomaker <jocdelavalle@gmail.com>
2022-08-15 09:12:59 -0500

    carusel

--- src/app/core/features/carussel-avatares/carussel-avatares.component.ts
+++ src/app/core/features/carussel-avatares/carussel-avatares.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarusselAvataresComponent implements OnInit {
 
+  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+
   constructor() { }
 
   ngOnInit(): void {
+    this.slides[0] = {
+      src: '../../../../assets/images/avatar/avatar1.png',
+    };
+    this.slides[1] = {
+      src: '../../../../assets/images/avatar/avatar2.png',
+    }
+    this.slides[2] = {
+      src: '../../../../assets/images/avatar/avatar1.png',
+    }
   }
 
 }
