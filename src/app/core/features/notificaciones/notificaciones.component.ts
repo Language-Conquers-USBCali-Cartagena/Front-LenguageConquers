28cb9e179fcaab6e2eea8627ca6afeb6ca71788c
Antomaker <96790352+Antomaker@users.noreply.github.com>
2022-10-04 17:51:19 -0500

    notificaciones

--- src/app/core/features/notificaciones/notificaciones.component.ts
+++ src/app/core/features/notificaciones/notificaciones.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notificaciones.component.css']
 })
 export class NotificacionesComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
